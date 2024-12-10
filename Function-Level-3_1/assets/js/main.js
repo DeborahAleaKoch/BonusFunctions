f7f72d870ac311d9847fe58dce32de063d152f53
DeborahAleaKoch <alea-de@web.de>
2024-12-10 15:28:56 +0100

    it's so lovely colorful🥰

---
+++ Function-Level-3_1/assets/js/main.js
@@ -0,0 +1,22 @@
+const meineH1 = document.querySelector("h1");
+const bodyHtml = document.querySelector("body");
+
+function changeColor1() {
+  meineH1.style.color = "rgba(68, 253, 45, 1)";
+  bodyHtml.style.backgroundColor = "rgba(195, 34, 193, 1)";
+}
+
+function changeColor2() {
+  meineH1.style.color = "rgba(253, 187, 45, 1)";
+  bodyHtml.style.backgroundColor = "rgba(34, 193, 195, 1)";
+}
+
+function changeColor3() {
+  meineH1.style.color = "rgba(42, 34, 11, 1)";
+  bodyHtml.style.backgroundColor = "rgba(255, 155, 0, 1)";
+}
+
+function changeColor4() {
+  meineH1.style.color = "rgba(24, 35, 92, 0.8785889355742297)";
+  bodyHtml.style.backgroundColor = "rgba(0, 234, 255, 1)";
+}
